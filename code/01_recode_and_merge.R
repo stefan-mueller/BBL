5da197b0db088fba250e038941540384711904e1
Stefan Müller <mullers@tcd.ie>
2017-06-16 22:59:51 +0100

    Update plot

--- code/01_recode_and_merge.R
+++ code/01_recode_and_merge.R
@@ -374,7 +374,7 @@ ggplot(dta_final_compare_years, aes(x = season, y = mean_type,
   ggtitle("Anteil der im Kader verbliebenen eingesetzten Spieler\n(vereinsübergreifend pro Saison)") +
   theme_bw() +
   theme(legend.position = "bottom")
-ggsave("output/comparison_per_season.jpg", height = 9, width = 10)
+ggsave("output/comparison_per_season.jpg", height = 5, width = 7.5)
 
 ## Load ratios reported by Baskets Bonn
 
